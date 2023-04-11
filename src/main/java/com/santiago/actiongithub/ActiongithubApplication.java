1faf1c88c8ba2f146c73efe6c152fd0b8e8a26b8
santiago <66737901+santy-ramirez@users.noreply.github.com>
2023-04-11 14:56:07 -0300

    deploy nº 3

--- src/main/java/com/santiago/actiongithub/ActiongithubApplication.java
+++ src/main/java/com/santiago/actiongithub/ActiongithubApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
 @SpringBootApplication
 @RestController
 @RequestMapping("/")
